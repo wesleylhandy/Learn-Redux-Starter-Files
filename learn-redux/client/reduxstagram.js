a608afbcaf08faf76601136f2245eae1526db160
wesleylhandy <wesleylhandy@gmail.com>
2017-12-14 17:11:39 -0500

    finished video 8, set up react router and defined actions

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,32 @@
 // let's go!
+import React from 'react';
+import {render} from 'react-dom';
+
+// import CSS
+import css from './styles/style.styl';
+
+//import components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/Photogrid';
+
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store.js';
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path='/view/:postId' component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+
+
+render(router, document.getElementById('root'));
+
